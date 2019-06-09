29a4e9341c48e90c645b6b5584cc27d4b4c41068
fluffywaffles <skorlir@gmail.com>
2019-06-09 17:25:29 -0500

    st: dmenu utf-8 and box-drawing patches

--- st-config.h
+++ st-config.h
@@ -9,6 +9,18 @@ static char *font = "monospace:size=9:anitalias=true:autohint=true";
 static int borderpx = 0;
 
 /*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 1;
+const int boxdraw_bold = 1;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
+
+/*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
  * 2: utmp option
