449c51555964f5f87536c397604aaa3e2291eca3
fluffywaffles <skorlir@gmail.com>
2019-06-10 16:29:13 -0500

    st: fix misspelling of 'antialias'

--- st-config.h
+++ st-config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=9:anitalias=true:autohint=true";
+static char *font = "monospace:size=9:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
